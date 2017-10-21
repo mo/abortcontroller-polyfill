2f47e9502eff87ccef0302077cc42d49e3dcd327
Martin Olsson <martin@minimum.se>
2017-10-21 22:41:45 +0200

    Update ES5 code in dist/

--- dist/browser-polyfill.js
+++ dist/browser-polyfill.js
@@ -90,4 +90,62 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
   self.AbortController = AbortController;
   self.AbortSignal = AbortSignal;
+})(typeof self !== 'undefined' ? self : undefined);'use strict';
+
+(function (self) {
+  'use strict';
+
+  if (!self.fetch) {
+    console.warn('fetch() is not available, cannot install abortcontroller-polyfill');
+    return;
+  }
+
+  // Note that the "unfetch" minimal fetch polyfill defines fetch() without
+  // defining window.Request, and this polyfill need to work on top of unfetch
+  // so the below feature detection is wrapped in "if (self.Request) { ... }".
+  if (self.Request) {
+    // Do feature detecting
+    var controller = new AbortController();
+    var signal = controller.signal;
+    var request = new Request('/', { signal: signal });
+
+    // Browser already supports abortable fetch (like FF v57 and fetch-polyfill)
+    if (request.signal) {
+      return;
+    }
+  }
+
+  var realFetch = fetch;
+  var abortableFetch = function abortableFetch(input, init) {
+    if (init && init.signal) {
+      var abortError = void 0;
+      try {
+        abortError = new DOMException('Aborted', 'AbortError');
+      } catch (err) {
+        // IE 11 does not support calling the DOMException constructor, use a
+        // regular error object on it instead.
+        abortError = new Error('Aborted');
+        abortError.name = 'AbortError';
+      }
+
+      // Return early if already aborted, thus avoiding making an HTTP request
+      if (init.signal.aborted) {
+        return Promise.reject(abortError);
+      }
+
+      // Turn an event into a promise, reject it once `abort` is dispatched
+      var cancellation = new Promise(function (_, reject) {
+        init.signal.addEventListener('abort', function () {
+          return reject(abortError);
+        }, { once: true });
+      });
+
+      // Return the fastest promise (don't need to wait for request to finish)
+      return Promise.race([cancellation, realFetch(input, init)]);
+    }
+
+    return realFetch(input, init);
+  };
+
+  self.fetch = abortableFetch;
 })(typeof self !== 'undefined' ? self : undefined);
