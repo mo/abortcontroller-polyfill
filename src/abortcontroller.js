fad7644d45afc974c13eae3b4e93e6baf8a91f1b
Martin Olsson <martin@minimum.se>
2018-02-04 18:40:20 +0100

    Minor style fixes for consistency

--- src/abortcontroller.js
+++ src/abortcontroller.js
@@ -6,28 +6,28 @@
   }
 
   class Emitter {
-    constructor () {
+    constructor() {
       this.listeners = {};
     }
-    addEventListener (type, callback) {
+    addEventListener(type, callback) {
       if (!(type in this.listeners)) {
         this.listeners[type] = [];
       }
       this.listeners[type].push(callback);
     }
-    removeEventListener (type, callback) {
+    removeEventListener(type, callback) {
       if (!(type in this.listeners)) {
         return;
       }
       const stack = this.listeners[type];
       for (let i = 0, l = stack.length; i < l; i++) {
-        if (stack[i] === callback){
+        if (stack[i] === callback) {
           stack.splice(i, 1);
           return;
         }
       }
     }
-    dispatchEvent (event) {
+    dispatchEvent(event) {
       if (!(event.type in this.listeners)) {
         return;
       }
