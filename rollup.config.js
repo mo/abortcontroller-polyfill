6420822cd23f6d99e0b8d46b465a5eb924754a5c
caub <cyril.auburtin@gmail.com>
2018-02-06 12:39:29 +0100

    rollupify

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import babel from 'rollup-plugin-babel';
+
+const plugins = [
+  babel({
+    exclude: 'node_modules/**'
+  })
+];
+
+export default [{
+  input: 'src/polyfill.js',
+  output: {
+    name: 'AbortController-polyfill',
+    file: 'dist/browser-polyfill.js',
+    format: 'umd'
+  },
+  plugins
+}, {
+  input: 'src/abortcontroller.js',
+  output: {
+    file: 'dist/abortcontroller.js',
+    format: 'cjs'
+  },
+  plugins
+}, {
+  input: 'src/patch-fetch.js',
+  output: {
+    file: 'dist/patch-fetch.js',
+    format: 'cjs'
+  },
+  plugins
+}];
--- src/abortcontroller.js
+++ src/abortcontroller.js
@@ -1,106 +1,94 @@
-(function(self) {
-  'use strict';
-
-  if (self.AbortController) {
-    return;
+class Emitter {
+  constructor() {
+    this.listeners = {};
   }
-
-  class Emitter {
-    constructor() {
-      this.listeners = {};
+  addEventListener(type, callback) {
+    if (!(type in this.listeners)) {
+      this.listeners[type] = [];
     }
-    addEventListener(type, callback) {
-      if (!(type in this.listeners)) {
-        this.listeners[type] = [];
-      }
-      this.listeners[type].push(callback);
+    this.listeners[type].push(callback);
+  }
+  removeEventListener(type, callback) {
+    if (!(type in this.listeners)) {
+      return;
     }
-    removeEventListener(type, callback) {
-      if (!(type in this.listeners)) {
+    const stack = this.listeners[type];
+    for (let i = 0, l = stack.length; i < l; i++) {
+      if (stack[i] === callback) {
+        stack.splice(i, 1);
         return;
       }
-      const stack = this.listeners[type];
-      for (let i = 0, l = stack.length; i < l; i++) {
-        if (stack[i] === callback) {
-          stack.splice(i, 1);
-          return;
-        }
-      }
-    }
-    dispatchEvent(event) {
-      if (!(event.type in this.listeners)) {
-        return;
-      }
-      const debounce = callback => {
-        setTimeout(() => callback.call(this, event));
-      };
-      const stack = this.listeners[event.type];
-      for (let i = 0, l = stack.length; i < l; i++) {
-        debounce(stack[i]);
-      }
-      return !event.defaultPrevented;
     }
   }
-
-  class AbortSignal extends Emitter {
-    constructor() {
-      super();
-
-      this.aborted = false;
-      this.onabort = null;
+  dispatchEvent(event) {
+    if (!(event.type in this.listeners)) {
+      return;
     }
-    toString() {
-      return '[object AbortSignal]';
+    const debounce = callback => {
+      setTimeout(() => callback.call(this, event));
+    };
+    const stack = this.listeners[event.type];
+    for (let i = 0, l = stack.length; i < l; i++) {
+      debounce(stack[i]);
     }
-    dispatchEvent(event) {
-      if (event.type === 'abort') {
-        this.aborted = true;
-        if (typeof this.onabort === 'function') {
-          this.onabort.call(this, event);
-        }
+    return !event.defaultPrevented;
+  }
+}
+
+export class AbortSignal extends Emitter {
+  constructor() {
+    super();
+
+    this.aborted = false;
+    this.onabort = null;
+  }
+  toString() {
+    return '[object AbortSignal]';
+  }
+  dispatchEvent(event) {
+    if (event.type === 'abort') {
+      this.aborted = true;
+      if (typeof this.onabort === 'function') {
+        this.onabort.call(this, event);
       }
-
-      super.dispatchEvent(event);
     }
+
+    super.dispatchEvent(event);
   }
+}
 
-  class AbortController {
-    constructor() {
-      this.signal = new AbortSignal();
-    }
-    abort() {
-      let event;
-      try {
-        event = new Event('abort');
-      } catch (e) {
-        if (typeof document !== 'undefined') {
-          // For Internet Explorer 11:
-          event = document.createEvent('Event');
-          event.initEvent('abort', false, false);
-        } else {
-          // Fallback where document isn't available:
-          event = {
-            type: 'abort',
-            bubbles: false,
-            cancelable: false
-          };
-        }
+export class AbortController {
+  constructor() {
+    this.signal = new AbortSignal();
+  }
+  abort() {
+    let event;
+    try {
+      event = new Event('abort');
+    } catch (e) {
+      if (typeof document !== 'undefined') {
+        // For Internet Explorer 11:
+        event = document.createEvent('Event');
+        event.initEvent('abort', false, false);
+      } else {
+        // Fallback where document isn't available:
+        event = {
+          type: 'abort',
+          bubbles: false,
+          cancelable: false
+        };
       }
-      this.signal.dispatchEvent(event);
     }
-    toString() {
-      return '[object AbortController]';
-    }
+    this.signal.dispatchEvent(event);
   }
-
-  if (typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-    // These are necessary to make sure that we get correct output for:
-    // Object.prototype.toString.call(new AbortController())
-    AbortController.prototype[Symbol.toStringTag] = 'AbortController';
-    AbortSignal.prototype[Symbol.toStringTag] = 'AbortSignal';
+  toString() {
+    return '[object AbortController]';
   }
+}
 
-  self.AbortController = AbortController;
-  self.AbortSignal = AbortSignal;
-
-})(typeof self !== 'undefined' ? self : this);
+if (typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+  // These are necessary to make sure that we get correct output for:
+  // Object.prototype.toString.call(new AbortController())
+  AbortController.prototype[Symbol.toStringTag] = 'AbortController';
+  AbortSignal.prototype[Symbol.toStringTag] = 'AbortSignal';
+}
