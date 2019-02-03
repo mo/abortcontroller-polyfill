814868c8bf294cbdb380fbe1be913d985c63e92c
Martin Olsson <martin@minimum.se>
2019-02-03 15:32:05 +0100

    Detect and polyfill over present-but-broken native AbortController in Safari 12.0.x, 12.1.x specifically but not >= 12.2
    The native AbortController in Safari 12.2 and above works (starting with Safari Technology Preview Release 74) so the
    polyfill should not install for these versions.
    
    The latest versions of Chrome/Firefox for IOS still have the "present but broken" native AbortController so always
    polyfill over their native implementations for now (the symptom otherwise is that cancelling never works, the HTTP
    call happens even if the .abort() happens before the fetch() and the fetch promise is not rejected.
    
    This commit fixes github issue #26

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,17 @@
+function hasBrokenNativeAbortController(userAgent) {
+  if (userAgent.match(/ (crios|gsa|fxios)\//i)) {
+    return true;
+  }
+  if (userAgent.match(/ Safari\//i) && (
+    userAgent.match(/ Version\/12.0/i) ||
+    userAgent.match(/ Version\/12.1/i)
+  )) {
+    return true;
+  }
+  return false;
+}
+
 export function nativeAbortControllerIsBroken(self) {
-  return self.navigator && (
-    (self.navigator.vendor && self.navigator.vendor.startsWith('Apple Computer')) ||
-    (self.navigator.userAgent && self.navigator.userAgent.match(/ (crios|gsa|fxios)\//i))
-  );
+  return self.navigator && self.navigator.userAgent &&
+    hasBrokenNativeAbortController(self.navigator.userAgent);
 }
