85d97d0481228408e5dea5c7844229be9e9d04b6
Sai Srinivasan <srinivasansairam8@gmail.com>
2019-02-13 20:13:31 -0800

    solve the broken native implementation of the abortcontroller for safari on version 11

--- src/utils.js
+++ src/utils.js
@@ -2,10 +2,7 @@ function hasBrokenNativeAbortController(userAgent) {
   if (userAgent.match(/ (crios|gsa|fxios)\//i)) {
     return true;
   }
-  if (userAgent.match(/ Safari\//i) && (
-    userAgent.match(/ Version\/12.0/i) ||
-    userAgent.match(/ Version\/12.1/i)
-  )) {
+  if (userAgent.match(/ Safari\//i) && (userAgent.match(/ Version\/(12.0|12.1|11)/i))) {
     return true;
   }
   return false;
