ac7b273b2e7beb9cc5c7907866c4c11526e5876c
caub <cyril.auburtin@gmail.com>
2019-03-06 00:56:08 +0100

    Update dependencies, set babel preset-env to IE 8

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,7 @@ import babel from 'rollup-plugin-babel';
 
 const plugins = [
   babel({
-    exclude: 'node_modules/**',
-    plugins: ['external-helpers']
+    exclude: 'node_modules/**'
   })
 ];
 
