6d265b8a7a54de62e36f1c2f07bd26e7383aa52c
Martin Olsson <martin@minimum.se>
2020-07-29 22:08:18 +0200

    Upgrade all packages to the latest version again
    And also re-pin chromedriver/geckodriver to new versions in wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -7,14 +7,14 @@ const seleniumVersions = {
     chrome: {
       // check for more recent versions of chrome driver here:
       // https://chromedriver.storage.googleapis.com/index.html
-      version: '80.0.3987.16',
+      version: '83.0.4103.39',
       arch: process.arch,
       baseURL: 'https://chromedriver.storage.googleapis.com'
     },
     firefox: {
       // check for more recent versions of geckodriver here:
       // https://github.com/mozilla/geckodriver/releases/
-      version: '0.26.0',
+      version: '0.27.0',
       arch: process.arch,
       baseURL: 'https://github.com/mozilla/geckodriver/releases/download'
     },
